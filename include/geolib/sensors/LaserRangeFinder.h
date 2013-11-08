b2dbdb660ad170dd3940b2d5e1aace716bc0cc49
Sjoerd van den Dries <s.v.d.dries@tue.nl>
2013-11-08 07:53:38 +0000

    Working on LRF class
    git-svn-id: https://roboticssrv.wtb.tue.nl/svn/ros/code/tue-ros-pkg/trunk/geolib@19618 3a3c2cfb-d3b9-409e-9e75-db8fcdbcaac6

---
+++ include/geolib/sensors/LaserRangeFinder.h
@@ -0,0 +1,56 @@
+#ifndef GEOLIB_LASERRANGEFINDER_H_
+#define GEOLIB_LASERRANGEFINDER_H_
+
+#include "geolib/Ray.h"
+
+namespace geo {
+
+class LaserRangeFinder {
+
+public:
+
+    LaserRangeFinder();
+
+    virtual ~LaserRangeFinder();
+
+    void render(const Shape& shape, const Pose3D& cam_pose, const Pose3D& obj_pose, std::vector<double>& ranges) const;
+
+    void setAngleLimits(double min, double max);
+
+    void setRangeLimits(double min, double max);
+
+    void setNumBeams(int n);
+
+    double getAngleMin() const;
+
+    double getAngleMax() const;
+
+    double getAngleIncrement() const;
+
+    double getRangeMin() const;
+
+    double getRangeMax() const;
+
+    int getNumBeams() const;
+
+    static geo::Vector3 polarTo2D(double angle, double range);
+
+    static geo::Vector3 polarTo3D(const geo::Pose3D& laser_pose, double angle, double range);
+
+protected:
+
+    double a_min_, a_max_;
+
+    double range_min_, range_max_;
+
+    int num_beams_;
+
+    std::vector<geo::Vector3> ray_dirs_;
+
+    void calculateRays();
+
+};
+
+}
+
+#endif
---
+++ src/sensors/LaserRangeFinder.cpp
@@ -0,0 +1,97 @@
+#include "geolib/sensors/LaserRangeFinder.h"
+#include "geolib/Shape.h"
+
+namespace geo {
+
+LaserRangeFinder::LaserRangeFinder() : a_min_(0), a_max_(0), num_beams_(0) {
+}
+
+LaserRangeFinder::~LaserRangeFinder() {
+}
+
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+//
+//                                        RAYTRACING
+//
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+
+void LaserRangeFinder::render(const Shape& shape, const Pose3D& cam_pose, const Pose3D& obj_pose, std::vector<double>& ranges) const {
+    tf::Transform t = cam_pose.inverse() * obj_pose;
+
+    ranges.resize(ray_dirs_.size());
+    for(unsigned int i = 0; i < ray_dirs_.size(); ++i) {
+        geo::Vector3 dir_t = t.getBasis() * ray_dirs_[i];
+        Ray r_t(t.getOrigin(), dir_t);
+
+        double distance = 0;
+        if (shape.intersect(r_t, range_min_, range_max_, distance)) {
+            ranges[i] = distance;
+        } else {
+            ranges[i] = 0;
+        }
+    }
+}
+
+void LaserRangeFinder::setAngleLimits(double min, double max) {
+    a_min_ = min;
+    a_max_ = max;
+    if (num_beams_ > 0 && a_max_ - a_min_ > 0) {
+        calculateRays();
+    }
+}
+
+void LaserRangeFinder::setRangeLimits(double min, double max) {
+    range_min_ = min;
+    range_max_ = max;
+}
+
+void LaserRangeFinder::setNumBeams(int num_beams) {
+    num_beams_ = num_beams;
+    if (num_beams > 0 && a_max_ - a_min_ > 0) {
+        calculateRays();
+    }
+}
+
+void LaserRangeFinder::calculateRays() {
+    ray_dirs_.clear();
+    double a_incr = getAngleIncrement();
+    double a = a_min_;
+    for(int i = 0; i < num_beams_; ++i) {
+        ray_dirs_.push_back(polarTo2D(a, 1));
+        a += a_incr;
+    }
+}
+
+double LaserRangeFinder::getAngleMin() const {
+    return a_min_;
+}
+
+double LaserRangeFinder::getAngleMax() const {
+    return a_max_;
+}
+
+double LaserRangeFinder::getAngleIncrement() const {
+    return (a_max_ - a_min_) / num_beams_;
+}
+
+double LaserRangeFinder::getRangeMin() const {
+    return range_min_;
+}
+
+double LaserRangeFinder::getRangeMax() const {
+    return range_max_;
+}
+
+int LaserRangeFinder::getNumBeams() const {
+    return num_beams_;
+}
+
+geo::Vector3 LaserRangeFinder::polarTo2D(double angle, double range) {
+    return geo::Vector3(sin(angle), cos(angle), 0) * range;
+}
+
+geo::Vector3 LaserRangeFinder::polarTo3D(const geo::Pose3D& laser_pose, double angle, double range) {
+    return laser_pose.getBasis() * polarTo2D(angle, range);
+}
+
+}
