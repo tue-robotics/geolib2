bb09c2e19e4ba8d734ef0837ae4b924a4aa1db32
Sjoerd van den Dries <s.v.d.dries@tue.nl>
2015-01-15 19:14:41 +0100

    Added conversion methods

--- include/geolib/ros/msg_conversions.h
+++ include/geolib/ros/msg_conversions.h
@@ -4,6 +4,7 @@
 #include "geolib/datatypes.h"
 #include <geometry_msgs/Pose.h>
 #include <geometry_msgs/Point32.h>
+#include <geometry_msgs/Transform.h>
 
 namespace geo {
 
@@ -14,6 +15,10 @@ inline void convert(const geo::Vector3& v, geometry_msgs::Point& msg) {
 }
 
 inline void convert(const geo::Vector3& v, geometry_msgs::Point32& msg) {
+    msg.x = v.x; msg.y = v.y; msg.z = v.z;
+}
+
+inline void convert(const geo::Vector3& v, geometry_msgs::Vector3& msg) {
     msg.x = v.x; msg.y = v.y; msg.z = v.z;
 }
 
@@ -32,6 +37,11 @@ inline void convert(const geo::Transform& t, geometry_msgs::Pose& msg) {
     convert(t.getBasis(), msg.orientation);
 }
 
+inline void convert(const geo::Transform& t, geometry_msgs::Transform& msg) {
+    convert(t.getOrigin(), msg.translation);
+    convert(t.getBasis(), msg.rotation);
+}
+
 // ------------------------------ FROM ROS ------------------------------
 
 inline void convert(const geometry_msgs::Point& msg, geo::Vector3& v) {
@@ -39,6 +49,10 @@ inline void convert(const geometry_msgs::Point& msg, geo::Vector3& v) {
 }
 
 inline void convert(const geometry_msgs::Point32& msg, geo::Vector3& v) {
+   v.x = msg.x; v.y = msg.y; v.z = msg.z;
+}
+
+inline void convert(const geometry_msgs::Vector3& msg, geo::Vector3& v) {
    v.x = msg.x; v.y = msg.y; v.z = msg.z;
 }
 
@@ -55,6 +69,11 @@ inline void convert(const geometry_msgs::Quaternion& msg, geo::Matrix3& r) {
 inline void convert(const geometry_msgs::Pose& msg, geo::Transform& t) {
     convert(msg.orientation, t.R);
     convert(msg.position, t.t);
+}
+
+inline void convert(const geometry_msgs::Transform& msg, geo::Transform& t) {
+    convert(msg.rotation, t.R);
+    convert(msg.translation, t.t);
 }
 
 }
