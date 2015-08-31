4a7300960f39b702f6e2b5c47c02cd2a7b2d8d4d
Albert Hofkamp <a.t.hofkamp@tue.nl>
2015-08-31 14:34:11 +0200

    Add: Method documentation, unduplicate point adding.

--- include/geolib/Mesh.h
+++ include/geolib/Mesh.h
@@ -5,13 +5,15 @@
 
 namespace geo {
 
+/** Triangle mesh, as triplet of indices in the point vector. */
 struct TriangleI {
 
     TriangleI(int i1, int i2, int i3) : i1_(i1), i2_(i2), i3_(i3) {}
 
-    int i1_, i2_, i3_;
+    int i1_, i2_, i3_; ///< Points in the Mesh::points_ vector.
 };
 
+/** Mesh storage. */
 class Mesh {
 
 public:
@@ -32,10 +34,19 @@ public:
 
     void copyAndRemoveTriangles(const std::set<int>& indices, geo::Mesh& target) const;
 
+    /**
+     * Test whether the mesh is empty.
+     * @return True if there are no triangles in the mesh (else false).
+     */
     inline bool empty() const { return triangles_i_.empty(); }
 
+    /**
+     * Get the number of triangles of the mesh.
+     * @return The number of triangles in the mesh.
+     */
     inline std::size_t size() const { return triangles_i_.size(); }
 
+    /** Empty the mesh. */
     inline void clear() {
         triangles_i_.clear();
         points_.clear();
@@ -55,13 +66,13 @@ public:
 
 protected:
 
-    mutable double max_radius_;
+    mutable double max_radius_; ///< Cached maximum radius.
 
-    std::vector<geo::Vector3> points_;
+    std::vector<geo::Vector3> points_; ///< Points of the mesh.
 
-    std::vector<TriangleI> triangles_i_;
+    std::vector<TriangleI> triangles_i_; ///< Triangles of the mesh.
 
-    mutable std::vector<Triangle> triangles_;
+    mutable std::vector<Triangle> triangles_; ///< Cached output result.
 
     inline int foo(int i, const std::vector<geo::Vector3>& vertices, std::vector<int>& vertex_mapping, std::vector<geo::Vector3>& vertices_new) const {
         if (vertex_mapping[i] >= 0) {
