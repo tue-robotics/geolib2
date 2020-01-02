62dbc0c06af29ee71e22589b46e77035588bac08
PetervDooren <19806646+PetervDooren@users.noreply.github.com>
2020-01-02 10:29:42 +0100

    style change
    Co-Authored-By: Matthijs van der Burgh <MatthijsBurgh@outlook.com>

--- src/Shape.cpp
+++ src/Shape.cpp
@@ -44,7 +44,7 @@ bool Shape::intersect(const Vector3& p) const {
 
         Vector3 e1_U = v1-v2;
         Vector3 e2_U = v2 - v3;
-        Vector3 e3_U = v3-v1;
+        Vector3 e3_U = v3 - v1;
 
         Vector3 e1_V = v1.cross(v2);
         Vector3 e2_V = v2.cross(v3);
