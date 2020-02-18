8304736d9b03d2ac0872250ced954a8c3306fa23
Peter van Dooren <pepevandooren@hotmail.com>
2020-02-18 16:59:13 +0100

    improved testing

--- test/test_intersect.cpp
+++ test/test_intersect.cpp
@@ -33,18 +33,19 @@ TEST(TestSuite, testCase2){
     geo::Vector3 max1(0.5, 0.5, 0.5);
     geo::Box box1(min1, max1);
 
-    geo::Pose3D p1(0.5, 0, 0);
-    geo::Pose3D p2(-0.5, 0, 0);
-    comp.addShape(box1, p1);
-    comp.addShape(box1, p2);
-
-    geo::Pose3D p3(0.0, 0.0, 0.0);
-    geo::Pose3D p4(10.0, 0.0, 0.0);
+    geo::Pose3D p1(0.1, 0.1, 0.1); // easy offset pose
+    geo::Pose3D p2(0.0, 0.0, 0.0); // exact overlap
+    geo::Pose3D p3(1.0, 0.0, 0.0); // meet at one face == no intersect
+    geo::Pose3D p2(2.0, 0.0, 0.0); // far away == no intersect
+    geo::Pose3D p4(1.1, 0.0, 0.0, 0.0, 0.0, 1.05); // rotated pose which intersects
 
     shapeptr = &box1;
 
-    ASSERT_TRUE(shapeptr->intersect(p3, box1));
+    ASSERT_TRUE(shapeptr->intersect(p1, box1));
+    ASSERT_TRUE(shapeptr->intersect(p2, box1));
+    ASSERT_FALSE(shapeptr->intersect(p3, box1));
     ASSERT_FALSE(shapeptr->intersect(p4, box1));
+    ASSERT_TRUE(shapeptr->intersect(p5, box1));
 }
 
 int main(int argc, char **argv){
