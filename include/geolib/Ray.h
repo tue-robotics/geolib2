c7bbf04863b5564f0fc47c4b985cfc4afa08f683
Matthijs van der Burgh <MatthijsBurgh@outlook.com>
2022-02-24 16:40:25 +0100

    (Ray) make variables protected
    Otherwise change the direction make the inverse direction invalid

--- include/geolib/Ray.h
+++ include/geolib/Ray.h
@@ -3,6 +3,8 @@
 
 #include "datatypes.h"
 
+#include <array>
+
 namespace geo {
 
 class Ray {
@@ -11,15 +13,31 @@ public:
 
     Ray(const Vector3 &o, const Vector3 &d) ;
 
+    const Vector3& getOrigin() const { return origin_; }
+
+    const Vector3& getDirection() const { return direction_; }
+
+    const Vector3& getInvDirection() const { return inv_direction_; }
+
+    double getLength() const { return length_; }
+
+    const std::array<int, 3>& getSign() const { return sign_; }
+
+    void setLength(const double length) { length_ = length; }
+
+protected:
+
     Vector3 origin_;
 
     Vector3 direction_;
 
     Vector3 inv_direction_;
 
-    double length_;
+    double length_; // When the length is zero, the ray is infinite
 
-    int sign[3];
+    std::array<int, 3> sign_;
+
+    void calculateInvDirection();
 
 };
 
--- src/Ray.cpp
+++ src/Ray.cpp
@@ -2,15 +2,23 @@
 
 namespace geo {
 
-Ray::Ray(const Vector3 &o, const Vector3 &d)
-    : origin_(o),
-      direction_(d), // ASSUME d is normalized!
-      length_(0) {   // length_ == 0 means infinite ray
+Ray::Ray(const Vector3 &o, const Vector3 &d) : origin_(o), direction_(d.normalized()), length_(0)
+{
+    calculateInvDirection();
 
-    inv_direction_ = geo::Vector3(1/direction_.x, 1/direction_.y, 1/direction_.z);
-    sign[0] = (inv_direction_.x < 0);
-    sign[1] = (inv_direction_.y < 0);
-    sign[2] = (inv_direction_.z < 0);
+    sign_[0] = (direction_.x < 0);
+    sign_[1] = (direction_.y < 0);
+    sign_[2] = (direction_.z < 0);
+}
+
+void Ray::calculateInvDirection()
+{
+    // Hardcoded EPS
+    double inv_x = std::abs(direction_.x) < 1e-9 ? 0 : 1/direction_.x;
+    double inv_y = std::abs(direction_.y) < 1e-9 ? 0 : 1/direction_.y;
+    double inv_z = std::abs(direction_.z) < 1e-9 ? 0 : 1/direction_.z;
+
+    inv_direction_ = geo::Vector3(inv_x, inv_y, inv_z);
 }
 
 }
