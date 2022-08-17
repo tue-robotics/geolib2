6899acb86ab4a733fcb487edf1db6992097f5bac
Matthijs van der Burgh <MatthijsBurgh@outlook.com>
2022-08-17 15:41:31 +0200

    Fix doxygen warnings of Shape

--- src/Shape.cpp
+++ src/Shape.cpp
@@ -32,7 +32,7 @@ bool check_linesegment(const Vector3& p, const double radius2, const Vector3& a,
 }
 
 /**
- * @link https://members.loria.fr/SLazard/ARC-Visi3D/Pant-project/files/Line_Segment_Triangle.html
+ *\a https://members.loria.fr/SLazard/ARC-Visi3D/Pant-project/files/Line_Segment_Triangle.html
  */
 class LineSegment
 {
@@ -55,12 +55,12 @@ protected:
 typedef geo::LineSegment LS;
 
 /**
- * @brief Determining the direction in which \a \bold q passes around \a \bold p.
- * The direction is taken from looking from the base to tip of the vector \a \bold p.
+ * @brief Determining the direction in which \a \b q passes around \a \b p.
+ * The direction is taken from looking from the base to tip of the vector \a \b p.
  * Zero means intersection
- * Positive means \a \bold q passses clockwise around \a \bold p.
- * Negative means \a \bold q passes counter clockwise around \a \bold p.
- * @link https://members.loria.fr/SLazard/ARC-Visi3D/Pant-project/files/Line_Segment_Triangle.html
+ * Positive means \a \b q passses clockwise around \a \b p.
+ * Negative means \a \b q passes counter clockwise around \a \b p.
+ *\a https://members.loria.fr/SLazard/ARC-Visi3D/Pant-project/files/Line_Segment_Triangle.html
  * @param p First line
  * @param q Second line
  * @return side value
@@ -71,7 +71,7 @@ double side_product(const geo::LS& p, const geo::LS& q) {
 
 /**
  * @brief Check if linesegment does intersect with line
- * @link https://members.loria.fr/SLazard/ARC-Visi3D/Pant-project/files/Line_Segment_Triangle.html
+ *\a https://members.loria.fr/SLazard/ARC-Visi3D/Pant-project/files/Line_Segment_Triangle.html
  * @param l line
  * @param ls linesegment
  * @param outside A point outside the plane which contains both the line and the linesegment
@@ -92,7 +92,7 @@ bool line_linesegment_intersection(const geo::LS& l, const geo::LS& ls, const ge
 
 /**
  * @brief Check if a line that is in the same plane as the triangle does actually intersect with the triangle
- * @link https://members.loria.fr/SLazard/ARC-Visi3D/Pant-project/files/Line_Segment_Triangle.html
+ *\a https://members.loria.fr/SLazard/ARC-Visi3D/Pant-project/files/Line_Segment_Triangle.html
  * @param t Triangle
  * @param line line
  * @return Intersection or not
@@ -154,9 +154,9 @@ bool Shape::intersect(const Ray& /*r*/, float /*t0*/, float /*t1*/, double& /*di
 
 /**
  *  Main logic:
- *  @link http://citeseerx.ist.psu.edu/viewdoc/download?doi=10.1.1.49.9172&rep=rep1&type=pdf
+ * \a http://citeseerx.ist.psu.edu/viewdoc/download?doi=10.1.1.49.9172&rep=rep1&type=pdf
  *  Projection in triangle logic:
- *  @link https://www.baeldung.com/cs/check-if-point-is-in-2d-triangle#1-mathematical-idea-2
+ * \a https://www.baeldung.com/cs/check-if-point-is-in-2d-triangle#1-mathematical-idea-2
  **/
 bool Shape::intersect(const Vector3& p, const double radius) const {
     const Mesh& mesh = getMesh();
@@ -225,7 +225,7 @@ bool Shape::intersect(const Vector3& p, const double radius) const {
  *  Let the line segment P connect points p and an arbitrary point p_out outside of the shape
  *  We count the number of intersections between P and the shape. A positive number means point p is inside the shape.
  *  We use plucker coordinates to determine whether or not a triangle intersects line segment P.
- *  more details @link https://members.loria.fr/SLazard/ARC-Visi3D/Pant-project/files/Line_Segment_Triangle.html
+ *  more details \a https://members.loria.fr/SLazard/ARC-Visi3D/Pant-project/files/Line_Segment_Triangle.html
  **/
 bool Shape::contains(const Vector3& p) const {
     const Mesh& mesh = getMesh();
