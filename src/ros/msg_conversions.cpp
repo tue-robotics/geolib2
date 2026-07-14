688b36c6d66c307fa143ecfaad897d3f15da3269
Matthijs van der Burgh <MatthijsBurgh@outlook.com>
2026-07-14 19:32:11 +0200

    Fix clang_format

--- src/ros/msg_conversions.cpp
+++ src/ros/msg_conversions.cpp
@@ -3,9 +3,10 @@
 #include "geolib/Mesh.h"
 #include "geolib/sensors/DepthCamera.h"
 #include <geometry_msgs/msg/point.hpp>
+#include <sensor_msgs/msg/camera_info.hpp>
 #include <shape_msgs/msg/mesh.hpp>
 #include <shape_msgs/msg/mesh_triangle.hpp>
-#include <sensor_msgs/msg/camera_info.hpp>
+
 #include <vector>
 
 namespace geo
